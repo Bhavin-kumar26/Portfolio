a6a57257c2e2b529c481bdc356a5ec94583ccd7e
Bhavin-kumar26 <154827005+Bhavin-kumar26@users.noreply.github.com>
2024-08-05 18:06:47 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "./Button";
-import { CiLocationArrow1 } from "react-icons/ci";
+import { Link } from "react-scroll";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
   return (
@@ -12,11 +13,29 @@ const Hero = () => {
             dynamic web magic with react js
           </h5>
           <h1 className=" text-center text-[40px] md:text-5xl lg:text-6xl font-bold max-w-[50rem] mt-3 ">
-          Designing Tomorrow’s Web Experiences  <span className=" text-[#c5b3ef]">Today</span>
+            Designing Tomorrow’s Web Experiences{" "}
+            <span className=" text-[#c5b3ef]">Today</span>
           </h1>
-          <h2 className=" mt-6 text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-[1.3rem] font-medium ">Hi! I'm Bhavin Kumar, a Frontend Developer based in India</h2>
+          <h2 className=" mt-6 text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-[1.3rem] font-medium ">
+            Hi! I'm Bhavin Kumar, a Frontend Developer based in India
+          </h2>
           <div className=" mt-6 sm:mt-4">
-          <Button title={"Show my work"}/>
+            <Link 
+            to="projects" 
+            spy={true} 
+            smooth={true} 
+            offset={10} 
+            duration={1000} 
+            className=" relative inline-flex h-12 w-full md:w-60 overflow-hidden rounded-lg p-[1px] focus:outline-none">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span
+                className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+         bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2  `}
+              >
+                Show my work
+                <FaLocationArrow className=" text-lg" />
+              </span>
+            </Link>
           </div>
         </div>
       </div>
